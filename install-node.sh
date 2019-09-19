a063b0bf0fc022254e69595921ae9f752248cd6c
四月 <350375092@qq.com>
2019-09-19 10:31:38 +0800

    编辑k8s脚本

--- install-node.sh
+++ install-node.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
 TOKEN=$1
+
 MASTER_IP=$2
+
+if [ ! $TOKEN ]; then
+	echo  'error:plase input [ $1 ] -- 请输入[ TOKEN ]'
+	exit
+fi
+
+if [ ! $MASTER_IP ]; then
+	echo  'error:plase input [ $2 ] -- 请输入[ MASTER_IP ]'
+	exit
+fi
 
 kubeadm join --token ${TOKEN} ${MASTER_IP}
 
